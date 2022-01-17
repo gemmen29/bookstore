38f250863b3c5982d07a3d7606d0aef07de21555
George Magdy <georgtriple@gmail.com>
2022-01-17 22:39:52 +0200

    Add book item component

---
+++ src/components/Books/BookItem/BookItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './BookItem.module.css';
+
+const BookItem = ({ book }) => (
+  <div className={styles.BookItem}>
+    {`Name: ${book.name}, Category: ${book.category}`}
+  </div>
+);
+
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+BookItem.defaultProps = {};
+
+export default BookItem;
