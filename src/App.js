b515fcfe081e07401b2e4aa9eb5b55afe45ec434
George Magdy <georgtriple@gmail.com>
2022-01-17 21:37:38 +0200

    Update UI and add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Books from './components/Books/Books';
+import Categories from './components/Categories/Categories';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/books" />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<Books />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
