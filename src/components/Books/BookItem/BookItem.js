a5e739168594a579496b180631fe28d1a9b7af2f
George Magdy <georgtriple@gmail.com>
2022-01-21 00:46:06 +0200

    Finish book item component style

--- src/components/Books/BookItem/BookItem.js
+++ src/components/Books/BookItem/BookItem.js
@@ -10,12 +10,61 @@ const BookItem = ({ book }) => {
     dispatch(removeBook(book.id));
   };
   return (
-    <div className={styles.BookItem}>
-      {`Name: ${book.title}, Category: ${book.category}`}
-      <button type="button" onClick={removeBookHandler}>
-        Remove
-      </button>
-    </div>
+    <section className="px-10 py-3">
+      <div className="grid grid-cols-4 gap-10 px-10 items-center shadow rounded border-2 py-6">
+        <div className="col-span-4 sm:col-span-2 lg:col-span-2">
+          <h3 className="text-gray-400 font-bold">{book.category}</h3>
+          <h2 className="font-bold text-xl">{book.title}</h2>
+          <h3 className="text-blue-400">Suzanne Collins</h3>
+          <div className="flex gap-3 py-2">
+            <button className="text-blue-400" type="button">
+              Comments
+            </button>
+            <button
+              className="text-blue-400 border-r-2 border-l-2 px-3"
+              type="button"
+              onClick={removeBookHandler}
+            >
+              Remove
+            </button>
+            <button className="text-blue-400" type="button">
+              Edit
+            </button>
+          </div>
+        </div>
+        <div className="flex gap-3 items-center col-span-4 sm:col-span-2 lg:col-span-1">
+          <svg width="100" height="100">
+            <circle
+              r="35"
+              cx="50"
+              cy="50
+          "
+              className={styles.track}
+            />
+            <circle
+              r="35"
+              cx="50"
+              cy="50"
+              className={`${styles['progress-circle']} ${styles['blue-circle']}`}
+            />
+          </svg>
+          <div>
+            <h6 className="text-5xl">8%</h6>
+            <p className="text-sm text-gray-400">Completed</p>
+          </div>
+        </div>
+        <div className="col-span-4 md:col-span-2 lg:col-span-1">
+          <h5 className="text-sm text-gray-400">Current Chapter</h5>
+          <h4 className="text-2xl">Chapter 3</h4>
+          <button
+            className="bg-sky-500 text-sm mt-3 col-span-2 md:col-span-1 hover:bg-blue-700 text-white py-2 px-8 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
